418fa10351a6e2f424818ff2a294c561bee3b0f4
Mawidev <savio.riccardo95@gmail.com>
2023-03-04 17:29:28 +0100

    fixed style

--- src/components/ResponseList.jsx
+++ src/components/ResponseList.jsx
@@ -1,9 +1,9 @@
 export default function ResponseList(props) {
     return (
       <div>
-        <ul className='mx-4 p-4 list-none flex flex-col gap-6'>
+        <ul className='p-4 list-none flex flex-col gap-6 bg-neutral-900'>
           {props.responses.slice().reverse().map((response, index) => (
-            <li className='border-2 border-neutral-400 rounded-md text-lg font-mono bg-neutral-500 text-neutral-900 p-4' key={index}>{response}</li>
+            <li className='mx-4 border-2 border-neutral-400 rounded-md text-lg font-mono bg-neutral-500 text-neutral-900 p-4' key={index}>{response}</li>
           ))}
         </ul>
       </div>
